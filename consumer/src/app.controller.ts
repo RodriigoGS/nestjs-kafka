5d4fb1f57c2e9541bb74e24190e7a007f10a2287
Rodrigo Silveira <gsrodriigo@gmail.com>
2022-06-26 20:27:51 -0300

    xpto.start reader

--- consumer/src/app.controller.ts
+++ consumer/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import {
+  Ctx,
+  KafkaContext,
+  MessagePattern,
+  Payload,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @MessagePattern('xpto.start')
+  readMessage(@Payload() message: any, @Ctx() context: KafkaContext) {
+    const response =
+      `Receiving a new message from topic: medium.rocks: ` +
+      JSON.stringify(context.getMessage().value);
+    console.log(response);
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    return response;
   }
 }
