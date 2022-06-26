dff5139d99a167c868b1ab6755277cf30bc420c7
Rodrigo Silveira <gsrodriigo@gmail.com>
2022-06-26 20:18:56 -0300

    register kafka client

--- consumer/src/app.module.ts
+++ consumer/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'consumer.nestjs',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'consumer.client-id',
+            brokers: ['localhost:29092'],
+          },
+          consumer: {
+            groupId: 'consumer.consumer.group-id',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
